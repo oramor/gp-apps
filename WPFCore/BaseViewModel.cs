5d9ada7536708bcfaa5b4fba4b1442ab4a1f53c7
Roman Rodin <romaro83@gmail.com>
2022-09-15 22:03:45 +0300

    add WPFCore lib

---
+++ WPFCore/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPFCore
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChaged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T property, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(property, value)) return false;
+            property = value;
+            OnPropertyChaged(PropertyName);
+            return true;
+        }
+    }
+}
