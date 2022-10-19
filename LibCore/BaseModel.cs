15ef98932f8f1ee28cea62edb6ebe000aea3c929
Roman Rodin <romaro83@gmail.com>
2022-10-20 01:16:26 +0300

    Backup

--- LibCore/BaseModel.cs
+++ LibCore/BaseModel.cs
@@ -1,6 +1,18 @@
 ﻿namespace LibCore
 {
-    public class BaseModel
+
+    public abstract class BaseModel
     {
+        //public abstract class Item { }
+
+        //public static async Task<object> GetItem()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public static async Task<List<object>> GetList()
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
