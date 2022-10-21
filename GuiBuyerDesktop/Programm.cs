1f3bb1143a6996a1faee7db3ff410c05274026cd
Roman R <romaro83@gmail.com>
2022-10-22 00:22:55 +0300

    Changed entry point

---
+++ GuiBuyerDesktop/Programm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiBuyerDesktop
+{
+    public class Programm
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
--- LibModels/Subject.cs
+++ LibModels/Subject.cs
@@ -6,64 +6,17 @@ using System.Threading.Tasks;
 
 namespace LibModels
 {
-    struct Endpoints
+    public enum SubjectStatus
     {
-        public readonly static string apiList = "/v1/subject/list";
-        public readonly static string apiItem = "/v1/subject/item";
+       active
     }
 
     public class Subject : BaseModel
     {
-        static Subject()
-        {
-            // Fetcher для обращения к API
-        }
-
-        public class Item
-        {
-            public int? Id { get; set; }
-            public string? Username { get; set; }
-            public string? Email { get; set; }
-            public string? Phone { get; set; }
-
-            public async void UpdatePhone(string phone)
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        // Этот метод будет асинхронно получать список
-        // экземпляров сущности с сервера
-        public static List<Item> GetList()
-        {
-            List<Item> list = new() {
-                GetItem(1),
-                GetItem(2),
-                GetItem(3)
-            };
-
-            return list;
-        }
-
-        // Этот метод будет запрашивать конкретный экземпляр
-        // сущности по API
-        public static Item GetItem(int id)
-        {
-            return new Item() {
-                Id = id,
-                Username = "ffff",
-                Email = "roror@ggg.ru",
-                Phone = "7878787878"
-            };
-        }
-
-        // Этот метод будет предпринимать асинхронную попытку
-        // аутентификации
-        public async Task<int> TryAuth(string login, string password)
-        {
-            //string json = "{\"subjectId\":1}";
-            Thread.Sleep(3);
-            return 1;
-        }
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public SubjectStatus Status { get; set; }
     }
 }
