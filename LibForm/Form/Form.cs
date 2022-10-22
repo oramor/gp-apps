b85104586fc7647a3a59f4299db2b33b0e8cef66
Roman Rodin <romaro83@gmail.com>
2022-10-22 19:22:07 +0300

    Backup

--- LibForm/Form/Form.cs
+++ LibForm/Form/Form.cs
@@ -3,16 +3,27 @@ using System.Windows.Controls;
 
 namespace LibForm
 {
-    public class Form : Control
+    public class Form : StackPanel
     {
         static Form()
         {
-            {
-                DefaultStyleKeyProperty.OverrideMetadata(
-                    typeof(Form),
-                    new FrameworkPropertyMetadata(typeof(Form))
-                    );
-            }
+
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(Form),
+                new FrameworkPropertyMetadata(typeof(Form))
+                );
+
+        }
+
+        public Form()
+        {
+            UIElementCollection children = InternalChildren;
+
+            var btn = new Button {
+                Content = "Test"
+            };
+
+            children.Add(btn);
         }
     }
 }
