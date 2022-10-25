5829985f9cbd53a565eae262441be66edd55edf6
Roman Rodin <romaro83@gmail.com>
2022-10-26 00:49:04 +0300

    Added errors to LoginForm

--- GuiBuyerDesktop/Windows/LoginWindow/LoginWindowContext.cs
+++ GuiBuyerDesktop/Windows/LoginWindow/LoginWindowContext.cs
@@ -1,12 +1,25 @@
 ﻿using LibForm;
-using System;
 
 namespace GuiBuyerDesktop.Windows.LoginWindow
 {
     internal class LoginWindowContext : BaseFormContext
     {
-        private string _login = String.Empty;
-        private string _password = String.Empty;
+        private string _login = string.Empty;
+        private string _loginError = string.Empty;
+        private string _password = string.Empty;
+        private string _passwordError = string.Empty;
+
+        public override bool IsFormReadyToSend
+        {
+            get {
+                return _login != string.Empty && _password != string.Empty;
+            }
+            set {
+                base.IsFormReadyToSend = value;
+            }
+        }
+
+        #region Login
 
         public string Login
         {
@@ -17,6 +30,16 @@ namespace GuiBuyerDesktop.Windows.LoginWindow
             }
         }
 
+        public string LoginError
+        {
+            get => _loginError;
+            set => Set(ref _loginError, value);
+        }
+
+        #endregion
+
+        #region Password
+
         public string Password
         {
             get => _password;
@@ -26,14 +49,12 @@ namespace GuiBuyerDesktop.Windows.LoginWindow
             }
         }
 
-        public override bool IsFormReadyToSend
+        public string PasswordError
         {
-            get {
-                return _login != String.Empty && _password != String.Empty;
-            }
-            set {
-                throw new NotSupportedException();
-            }
+            get => _passwordError;
+            set => Set(ref _passwordError, value);
         }
+
+        #endregion
     }
 }
