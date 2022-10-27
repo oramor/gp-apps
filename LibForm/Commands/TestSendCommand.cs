d8ee7e9080cc6d712db18f1cb72e08ee3c7b018b
Roman Rodin <romaro83@gmail.com>
2022-10-28 00:45:47 +0300

    Json parsing

--- LibForm/Commands/TestSendCommand.cs
+++ LibForm/Commands/TestSendCommand.cs
@@ -7,9 +7,51 @@ namespace LibForm
 {
     internal class TestSendCommand : BaseCommand
     {
-        struct Test
+        class ServerResponse
         {
-            public string Name;
+            public string Handler { get; set; } = string.Empty;
+            public FormResult Dto { get; set; } = new FormResult();
+        }
+
+        class FormResult
+        {
+            public string FormState { get; set; } = string.Empty;
+            public FormResultDTO FormDto { get; set; } = new FormResultDTO();
+
+        }
+
+        class FieldErrorInfo
+        {
+            public string Name { get; set; } = string.Empty;
+            public string Message { get; set; } = string.Empty;
+        }
+
+        class FormResultDTO
+        {
+            /// <summary>
+            /// Ошибка, которая должна отображаться в верхней части формы.
+            /// Есть предложение удалить это поле (см. сервер).
+            /// Относится к InvalidFormDTO
+            /// </summary>
+            public string? TopError { get; set; }
+
+            /// <summary>
+            /// Список полей, в которых обнаружена ошибка
+            /// Относится к InvalidFormDTO
+            /// </summary>
+            public FieldErrorInfo[]? Fields { get; set; }
+
+            /// <summary>
+            /// Сообщение успешной отправки формы. Относится к SuccessFormDTO
+            /// </summary>
+            public string? Title { get; set; }
+
+            /// <summary>
+            /// Содержит как результат ошибки в случае кода formError,
+            /// так и сообщение об успехе в случае кода formSuccess
+            /// </summary>
+            public string? Message { get; set; }
+
         }
 
         public async override void Execute(object? parameter)
@@ -17,7 +59,6 @@ namespace LibForm
             using (var client = new HttpClient())
             using (var content = new MultipartFormDataContent()) {
                 client.DefaultRequestHeaders.Add("Authorization", "ssdsd");
-                //var field = JsonContent.Create(new Test { Name = "dfdf" });
                 var fieldValue = new StringContent("fgff");
                 var fieldName = JsonNamingPolicy.CamelCase.ConvertName("Login");
                 var fieldValue2 = new StringContent("sssdddds");
@@ -27,7 +68,15 @@ namespace LibForm
 
                 var endpoint = new Uri("http://localhost/api/v1/subjects/login");
                 HttpResponseMessage rs = await client.PostAsync(endpoint, content);
-                var a = rs.Content;
+                var handlerCode = rs.Headers.GetValues("x-handler-code");
+                var httpContent = rs.Content;
+                string json = await httpContent.ReadAsStringAsync();
+
+                var options = new JsonSerializerOptions {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                };
+                ServerResponse formResult = JsonSerializer.Deserialize<ServerResponse>(json, options)!;
+                string formState = formResult.Dto.FormState;
             };
 
 
