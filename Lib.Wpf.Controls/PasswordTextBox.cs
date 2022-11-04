3fe63d26b9c1fc6ec01e403b1650eba97b18c338
Roman Rodin <romaro83@gmail.com>
2022-11-05 02:00:23 +0300

    Lib.Wpf.Controls

---
+++ Lib.Wpf.Controls/PasswordTextBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace Lib.Wpf.Controls
+{
+    public class PasswordTextBox : PlaceholderTextBox
+    {
+        static PasswordTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(PasswordTextBox),
+                new FrameworkPropertyMetadata(typeof(PasswordTextBox))
+                );
+        }
+
+        /// <summary>
+        /// Показать текст.
+        /// </summary>
+        public bool IsTextVisibility
+        {
+            get => (bool)GetValue(IsTextVisibilityProperty);
+            set => SetValue(IsTextVisibilityProperty, value);
+        }
+
+        /// <summary>
+        /// <see cref="DependencyProperty"/> для свойства <see cref="IsTextVisibility"/>.
+        /// </summary>
+        public static readonly DependencyProperty IsTextVisibilityProperty =
+            DependencyProperty.Register(
+                nameof(IsTextVisibility),
+                typeof(bool),
+                typeof(PasswordTextBox),
+                new PropertyMetadata(false));
+    }
+}
