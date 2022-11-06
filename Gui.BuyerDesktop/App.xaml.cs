7e4c20987a2bdd99392cd472fcea0a47878c8e56
Roman Rodin <romaro83@gmail.com>
2022-11-06 19:33:25 +0300

    GuiBuyerDesktop -> Gui.BuyerDesktop

---
+++ Gui.BuyerDesktop/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace Gui.BuyerDesktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static bool IsProductionMode => false;
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Произошла неизвестная ошибка: " + e.Exception.Message,
+                "Ошибка",
+                MessageBoxButton.OK,
+            MessageBoxImage.Error);
+
+            if (IsProductionMode)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ Gui.BuyerDesktop/Windows/LoginWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Gui.BuyerDesktop.Windows
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
