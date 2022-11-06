5d01a2dcbb0052d4ca10fe7b67985d7167ebf691
Roman Rodin <romaro83@gmail.com>
2022-11-06 19:38:15 +0300

    MainWindow draft

---
+++ Gui.BuyerDesktop/Windows/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Gui.BuyerDesktop.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
