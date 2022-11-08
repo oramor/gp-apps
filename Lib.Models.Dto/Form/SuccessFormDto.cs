14bd38e2b75e2ab050f69ef62a4ff1873c5856ff
Roman Rodin <romaro83@gmail.com>
2022-11-08 16:11:17 +0300

    Removed old files

--- Lib.Models.Dto/Form/SuccessFormDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lib.Models.Dto.Form
-{
-    // DTO содержит больше полей, но они пока не актуальны. Можно добавить TopMessage
-    public class SuccessFormDto
-    {
-        public string? Message { get; set; } = string.Empty;
-    }
-}
--- LibForm/Dto/SuccessFormDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LibForm.Dto
-{
-    // DTO содержит больше полей, но они пока не актуальны. Можно добавить TopMessage
-    public class SuccessFormDto
-    {
-        public string? Message { get; set; } = string.Empty;
-    }
-}
