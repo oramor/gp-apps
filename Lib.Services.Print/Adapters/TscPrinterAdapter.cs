5c89e380be82b43b4bc7060b302d8789779ff783
Roman R <romaro83@gmail.com>
2022-11-08 23:51:55 +0300

    Backup

---
+++ Lib.Services.Print/Adapters/TscPrinterAdapter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Services.Print.Adapters
+{
+    public static class TscPrinterAdapter
+    {
+        [DllImport("TSCLIB.dll", EntryPoint = "about")]
+        public static extern int about();
+
+        [DllImport("TSCLIB.dll", EntryPoint = "openport")]
+        public static extern int openport(string printername);
+
+        [DllImport("TSCLIB.dll", EntryPoint = "clearbuffer")]
+        public static extern int clearbuffer();
+    }
+}
