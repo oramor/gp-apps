e9386230df2dcbba455187cc5b9cced66b602742
Roman Rodin <romaro83@gmail.com>
2022-11-08 21:11:44 +0300

    Backup

--- Lib.Services/IPrintService.cs
+++ Lib.Services/IPrintService.cs
@@ -7,21 +7,21 @@
         /// которая задействует для этого параметры регистра (класс качества
         /// товара, шаблон упаковки, код товарной позиции и т.д.)
         /// </summary>
-        Task PrintIncomeBarcode(int sku);
+        Task PrintIncomeLabel(int sku);
     }
 
-    #region Barcode params
+    #region Label params
     /// <summary>
     /// Свойства этикеток для каждого маркетплейса различны. И даже при одинаковых
     /// свойствах могут различаться параметры (например, разная предельно допустимая
     /// длина для названия товара)
     /// </summary>
-    public interface IPrintService_OzonBarcodeParams
+    public interface IPrintService_OzonLabelParams
     {
         string ProductName { get; set; }
     }
 
-    public interface IPrintService_WbBarcodeParams
+    public interface IPrintService_WbLabelParams
     {
         string ProductName { get; set; }
     }
