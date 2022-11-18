e6b895b5de95e6ecdb1c17b1b6957f90de0e6294
Roman Rodin <romaro83@gmail.com>
2022-11-19 00:18:59 +0300

    Backup

--- Lib.Models/Base/BaseModel.cs
+++ Lib.Models/Base/BaseModel.cs
@@ -3,6 +3,7 @@
 
     public abstract class BaseModel
     {
+        public int Id { get; init; }
         //public abstract class Item { }
 
         //public static async Task<object> GetItem()
--- Lib.Models/Subject.cs
+++ Lib.Models/Subject.cs
@@ -8,7 +8,6 @@
 
     public class Subject : BaseModel
     {
-        public int Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
