1c87b35e4e4d9a8c55d4c56b99467b6999615c4e
Roman Rodin <romaro83@gmail.com>
2022-11-29 17:30:16 +0300

    Backup

--- Gui.BuyerDesktop/Contexts/Windows/MainWindowContext.cs
+++ Gui.BuyerDesktop/Contexts/Windows/MainWindowContext.cs
@@ -1,9 +1,14 @@
 ﻿using Lib.Wpf.Core;
+using System;
+using System.Windows.Input;
 
 namespace Gui.BuyerDesktop.Contexts
 {
-    internal interface IMainWindowContext : IWindowContext
+    internal interface IHaveCustomTabs<T> where T : System.Enum
     {
+        T CurrentTab { get; }
+        ICommand ChangeTabCmd { get; }
+        void ChangeTab(T targetTab);
     }
 
     internal enum MainWindowTabs
@@ -12,17 +17,52 @@ namespace Gui.BuyerDesktop.Contexts
         Printer
     }
 
+    internal interface IMainWindowContext : IWindowContext, IHaveCustomTabs<MainWindowTabs>
+    {
+    }
+
     internal class MainWindowContext : BaseContext, IMainWindowContext
     {
         private string _title = "Главное окно";
+        private MainWindowTabs _currentTab = MainWindowTabs.Printer;
 
-        public MainWindowTabs CurrentTab { get; private set; } = MainWindowTabs.Printer;
-        public bool Test { get; set; } = true;
+        public MainWindowTabs CurrentTab
+        {
+            get => _currentTab;
+            set => Set(ref _currentTab, value);
+        }
 
         public string Title
         {
             get => _title;
             set => Set(ref _title, value);
         }
+
+        public ICommand ChangeTabCmd => new ChangeTabCommand(this);
+
+        public void ChangeTab(MainWindowTabs targetTab)
+        {
+            CurrentTab = targetTab;
+        }
+
+        #region Commands Classes
+
+        private class ChangeTabCommand : BaseCommand
+        {
+            private IHaveCustomTabs<MainWindowTabs> _context;
+
+            public ChangeTabCommand(IHaveCustomTabs<MainWindowTabs> ctx)
+            {
+                _context = ctx;
+            }
+
+            public override void Execute(object? parameter)
+            {
+                var targetTab = (MainWindowTabs)Enum.Parse(typeof(MainWindowTabs), parameter as string);
+                _context.ChangeTab(targetTab);
+            }
+        }
+
+        #endregion
     }
 }
